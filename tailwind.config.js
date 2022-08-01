85f19a2e2e14d284415face600fac260a122f24e
Francie Jiang <i@francie.cc>
2022-08-01 17:55:36 +0800

    Welcome my new portfolio!

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{vue,js,html}",
+    "./index.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
